b6fff2c3acd391b55533355ec45db42cb95a0abf
abaas <arnaud.baas@enseirb-matmeca.fr>
2020-01-14 15:28:54 +0100

    manifest

--- generate_manifest/live.sh
+++ generate_manifest/live.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-sudo ffmpeg -re -i /dev/video0 -c:v libx264 -crf 21 -preset veryfast -c:a aac -b:a 128k -ac 2 -f hls -hls_time 4 -hls_playlist_type event stream.m3u8
-rm -rf stream.m3u8
+sudo ffmpeg -re -i /dev/video0 -c:v libx264 -crf 21 -preset veryfast -c:a aac -b:a 128k -ac 2 -f hls -hls_time 4 -hls_playlist_type event ../HLSproject/trombinoscop/static/video/live/stream.m3u8
+rm -rf ../HLSproject/trombinoscop/static/video/live/*
